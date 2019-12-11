fa7a12bf0d513e9f10d1d581a528044587f31dc5
lihanbing <lihanbing@163.com>
2019-12-11 21:46:28 +0800

    lhb

--- webclient/src/app/manager/table-template-field/table-template-field.component.ts
+++ webclient/src/app/manager/table-template-field/table-template-field.component.ts
@@ -8,24 +8,30 @@ import { ManagerBase } from 'src/app/wizard';
 })
 export class TableTemplateFieldComponent extends ManagerBase {
 
-  constructor() { super() }
+  constructor() { super() 
+    this.context['buttons'] = [{ key: 'delete', name: '删除' }];
+  }
 
   typename = "TableTemplateField"
 
   ngOnInit() {
     console.log(this.ds)
     if (this.typename) {
-      this.ds.data[this.typename + '_list'] = null
+      this.ds.data[this.typename + '_list'] = [{}]
       this.ds.data[this.typename] = null
-      this.ds.loadData(this.typename,"1");
+      //this.ds.loadData(this.typename,"1");
     }
   }
-
+  defaultColDef = {
+    editable: true,
+    resizable: true
+  }
   columnDefs = [
-    { headerName: '名称', field: 'Name' },
-    { headerName: '标题', field: 'Title' },
-    { headerName: '类型', field: 'Type' },
-    { headerName: '长度', field: 'Length' },
+    { headerName: "操作", field: '', cellRenderer: "childMessageRenderer",width:60 },
+    { headerName: '名称', field: 'Name' ,width:120 },
+    { headerName: '标题', field: 'Title' ,width:120 },
+    { headerName: '类型', field: 'Type' ,width:120 },
+    { headerName: '长度', field: 'Length' ,width:120 },
   ]
  
 
