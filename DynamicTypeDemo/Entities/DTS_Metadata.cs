9a1f1a91bcf3e8c5fe9f8686568e6d0b93e0418f
lihanbing <lihanbing@163.com>
2019-12-26 21:41:34 +0800

    lhb

---
+++ DynamicTypeDemo/Entities/DTS_Metadata.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicTypeDemo.Entities
+{
+    public class DTS_Metadata
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public PrimitiveTypeKind type { get; set; }
+        public int length { get; set; }
+
+    }
+}
